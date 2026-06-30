c6211cb0f848a532fc71154e8b1f60ce363c6498
poairtronic <posuppportairtronic@gmail.com>
2026-06-30 13:00:35 +0530

    fix: wrap naked JS comment in JSX braces in VendorRiskMatrix

--- src/components/forecasting/VendorRiskMatrix.jsx
+++ src/components/forecasting/VendorRiskMatrix.jsx
@@ -62,8 +62,8 @@ function VendorRiskMatrix() {
       </div>
 
 
-        /* Risk Matrix View */
-        <div style={{ overflowX: 'auto', maxHeight: 400, overflowY: 'auto' }}>
+      {/* Risk Matrix View */}
+      <div style={{ overflowX: 'auto', maxHeight: 400, overflowY: 'auto' }}>
           <table style={{ width: '100%', borderCollapse: 'collapse', fontSize: 12 }}>
             <thead>
               <tr>
